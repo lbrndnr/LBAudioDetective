7d50b1bd14b6a24fd4adc647e2ca9f8270b90c74
Laurin <larcus94@gmail.com>
2013-06-09 17:46:18 +0200

    Sample rate is differently determined on a mac

--- LBAudioDetective/LBAudioDetective.m
+++ LBAudioDetective/LBAudioDetective.m
@@ -115,9 +115,14 @@ void LBAudioDetectiveDispose(LBAudioDetectiveRef inDetective) {
 
 AudioStreamBasicDescription LBAudioDetectiveDefaultFormat() {
     Float64 defaultSampleRate;
+    
+#if defined(__IPHONE_OS_VERSION_MIN_REQUIRED)
     UInt32 propertySize = sizeof(Float64);
     OSStatus error = AudioSessionGetProperty(kAudioSessionProperty_CurrentHardwareSampleRate, &propertySize, &defaultSampleRate);
     LBErrorCheck(error);
+#else
+    defaultSampleRate = 44100.0;
+#endif
     
     UInt32 bytesPerSample = sizeof(SInt16);
     
